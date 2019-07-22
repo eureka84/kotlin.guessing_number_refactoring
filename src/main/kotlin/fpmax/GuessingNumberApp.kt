911e3add810153048149f0a1bb6f6d42da8b7f7f
eureka84 <angelosciarra@ymail.com>
2019-07-23 01:53:05 +0200

    rewrote with monad comprehension

--- src/main/kotlin/fpmax/GuessingNumberApp.kt
+++ src/main/kotlin/fpmax/GuessingNumberApp.kt
@@ -4,6 +4,8 @@ import arrow.core.Option
 import arrow.core.Try
 import arrow.core.getOrElse
 import arrow.effects.IO
+import arrow.effects.extensions.io.monad.monad
+import arrow.effects.fix
 import java.util.*
 
 
@@ -16,30 +18,25 @@ fun main(args: Array<String>) {
 }
 
 private fun program(random: Random): IO<Unit> =
-    putStrLine("What is your name?")
-        .flatMap { readStrLine() }
-        .flatMap { name ->
-            putStrLine("Hello, $name, welcome to the game!")
-                .flatMap { gameLoop(random, name) }
-        }
+    IO.monad().binding { // TODO FIXME using fx
+        putStrLine("What is your name?").bind()
+        val name = readStrLine().bind()
+        putStrLine("Hello, $name, welcome to the game!").bind()
+        gameLoop(random, name).bind()
+    }.fix()
 
 private fun gameLoop(random: Random, name: String?): IO<Unit> =
-    IO { random.nextInt(5) + 1 }
-        .flatMap { num ->
-            putStrLine("Dear $name, please guess a number from 1 to 5:")
-                .flatMap { readGuess() }
-                .map { guess -> guess.map { g -> g == num }.getOrElse { false } }
-                .flatMap { guessedRight ->
-                    if (guessedRight) putStrLine("You guessed right, $name!")
-                    else putStrLine("You guessed wrong, $name! The number was: $num")
-                }
-                .flatMap { putStrLine("Do you want to continue, $name?") }
-                .flatMap { checkContinue() }
-                .flatMap { cont ->
-                    if (cont) gameLoop(random, name)
-                    else IO { Unit }
-                }
-        }
+    IO.monad().binding { // TODO FIXME using fx
+        val num = IO { random.nextInt(5) + 1 }.bind()
+        putStrLine("Dear $name, please guess a number from 1 to 5:").bind()
+        val guessedRight = readGuess().bind().map { g -> g == num }.getOrElse { false }
+        if (guessedRight) putStrLine("You guessed right, $name!").bind()
+        else putStrLine("You guessed wrong, $name! The number was: $num").bind()
+        putStrLine("Do you want to continue, $name?").bind()
+        val cont = checkContinue().bind()
+        if (cont) gameLoop(random, name).bind()
+        else IO { Unit }.bind()
+    }.fix()
 
 fun putStrLine(message: String): IO<Unit> = IO { println(message) }
 fun readStrLine(): IO<String?> = IO { readLine() }
