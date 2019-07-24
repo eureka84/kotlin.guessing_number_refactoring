d1a1d205761fa5c8ad3c3264522619a61a6f6379
eureka84 <angelosciarra@ymail.com>
2019-07-24 19:25:04 +0200

    rewrote it generalizing the IO effect

---
+++ src/main/kotlin/fpmax/AbstractGuessingNumber.kt
@@ -0,0 +1,75 @@
+package fpmax
+
+import arrow.Kind
+import arrow.core.ForOption
+import arrow.core.Option
+import arrow.core.Try
+import arrow.core.extensions.option.traverse.sequence
+import arrow.typeclasses.Monad
+
+interface Console<E> {
+    fun writeLn(msg: String): Kind<E, Unit>
+    fun readLn(): Kind<E, String?>
+}
+
+interface CustomRandom<E> {
+    fun nextInt(upper: Int): Kind<E, Int>
+}
+
+class GuessingGame<E>(
+    private val console: Console<E>,
+    private val random: CustomRandom<E>,
+    private val monad: Monad<E>
+) {
+    fun play(): Kind<E, Unit> = monad.run {
+        console.writeLn("What is your name?")
+            .flatMap { console.readLn() }
+            .flatMap { name ->
+                console.writeLn("Hello, $name, welcome to the game!")
+                    .flatMap { gameLoop(name) }
+            }
+    }
+
+    private fun gameLoop(player: String?): Kind<E, Unit> =
+        monad.run {
+            random.nextInt(5)
+                .map { it + 1 }
+                .flatMap { num -> askPlayerToGuess(player, num) }
+        }
+
+    private fun askPlayerToGuess(name: String?, num: Int): Kind<E, Unit> =
+        monad.run {
+            console.writeLn("Dear $name, please guess a number from 1 to 5:")
+                .flatMap { readGuess() }
+                .flatMap { guess -> evaluateGuess(guess, num, name) }
+                .flatMap { console.writeLn("Do you want to continue, $name?") }
+                .flatMap { checkContinue({ monad.just(Unit) }, { gameLoop(name) }) }
+        }
+
+
+    private fun evaluateGuess(guess: Option<Int>, num: Int, name: String?): Kind<E, Kind<ForOption, Unit>> =
+        guess
+            .map { numberGuessed -> numberGuessed == num }
+            .map { guessedRight ->
+                if (guessedRight)
+                    console.writeLn("You guessed right, $name!")
+                else
+                    console.writeLn("You guessed wrong, $name! The number was: $num")
+            }.sequence(monad)
+
+    private fun readGuess(): Kind<E, Option<Int>> =
+        monad.run {
+            console.readLn().map { input -> Try { input!!.toInt() }.toOption() }
+        }
+
+    private fun checkContinue(ifNo: () -> Kind<E, Unit>, ifYes: () -> Kind<E, Unit>): Kind<E, Unit> =
+        monad.run {
+            console.readLn().flatMap { input ->
+                when (input?.toLowerCase()) {
+                    "y" -> ifYes()
+                    "n" -> ifNo()
+                    else -> ifYes()
+                }
+            }
+        }
+}
--- src/main/kotlin/fpmax/GuessingNumberApp.kt
+++ src/main/kotlin/fpmax/GuessingNumberApp.kt
@@ -1,13 +1,10 @@
 package fpmax
 
 import arrow.Kind
-import arrow.core.ForOption
-import arrow.core.Option
-import arrow.core.Try
-import arrow.core.extensions.option.traverse.sequence
 import arrow.effects.ForIO
 import arrow.effects.IO
-import arrow.effects.extensions.io.applicative.applicative
+import arrow.effects.extensions.io.monad.monad
+import arrow.effects.fix
 import java.util.*
 
 
@@ -15,49 +12,16 @@ fun main(args: Array<String>) {
     val seed = args[0].toLong()
     val random = Random(seed)
 
-    program(random).unsafeRunSync()
+    val program: IO<Unit> = GuessingGame(ConsoleIO, RandomIO(random), IO.monad()).play().fix()
 
+    program.unsafeRunSync()
 }
 
-private fun program(random: Random): IO<Unit> =
-    putStrLine("What is your name?")
-        .flatMap { readStrLine() }
-        .flatMap { name ->
-            putStrLine("Hello, $name, welcome to the game!")
-                .flatMap { gameLoop(random, name) }
-        }
+object ConsoleIO : Console<ForIO> {
+    override fun readLn(): Kind<ForIO, String?> = IO { readLine() }
+    override fun writeLn(msg: String): Kind<ForIO, Unit> = IO { println(msg) }
+}
 
-private fun gameLoop(random: Random, name: String?): IO<Unit> =
-    IO { random.nextInt(5) + 1 }
-        .flatMap { num ->
-            putStrLine("Dear $name, please guess a number from 1 to 5:")
-                .flatMap { readGuess() }
-                .flatMap { guess -> evaluateGuess(guess, num, name) }
-                .flatMap { putStrLine("Do you want to continue, $name?") }
-                .flatMap { checkContinue({ IO { Unit } }, { gameLoop(random, name) }) }
-        }
-
-private fun evaluateGuess(guess: Option<Int>, num: Int, name: String?): Kind<ForIO, Kind<ForOption, Unit>> =
-    guess
-        .map { numberGuessed -> numberGuessed == num }
-        .map { guessedRight ->
-            if (guessedRight)
-                putStrLine("You guessed right, $name!")
-            else
-                putStrLine("You guessed wrong, $name! The number was: $num")
-        }.sequence(IO.applicative())
-
-fun putStrLine(message: String): IO<Unit> = IO { println(message) }
-fun readStrLine(): IO<String?> = IO { readLine() }
-
-fun readGuess(): IO<Option<Int>> =
-    readStrLine().map { input -> Try { input!!.toInt() }.toOption() }
-
-fun checkContinue(ifNo: () -> IO<Unit>, ifYes: () -> IO<Unit>): IO<Unit> =
-    readStrLine().flatMap { input ->
-        when (input?.toLowerCase()) {
-            "y" -> ifYes()
-            "n" -> ifNo()
-            else -> ifYes()
-        }
-    }
+class RandomIO(private val random: Random) : CustomRandom<ForIO> {
+    override fun nextInt(upper: Int): Kind<ForIO, Int> = IO { random.nextInt(upper) }
+}
